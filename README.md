2fd87aa176c1d5c3435e000a08ee087782fc7007
李進興 Vulcan <vulcan@miniasp.com>
2019-04-23 11:23:47 +0800

    使用 StackLayout 配合 BindableLayout.ItemTemplateSelector 建立一個動態資料樣板的呈現的效果

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 |XF2001|ACR User Dialogs 功能展示||
 |XF3001|FlexLayout 的應用練習||
 |XF3002|CollectionView 的應用練習||
-||||
+|XF3003|StackLayout 的 BindableLayout.ItemTemplateSelector 應用練習
+||
 ||||
 ||||
 ||||
---
+++ XF3003/XF3003/XF3003/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF3003.ViewModels
+{
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    using XF3003.Models;
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<ItemBlock> myItemList { get; set; } = new ObservableCollection<ItemBlock>();
+        private readonly INavigationService navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            myItemList.Add(new ItemBlock()
+            {
+                ShowViewType = ItemBlockTypeEnum.Label,
+                LabelText = "User Account"
+            });
+            myItemList.Add(new ItemBlock()
+            {
+                ShowViewType = ItemBlockTypeEnum.Entry,
+                LabelText = "Please Enter Account"
+            });
+            myItemList.Add(new ItemBlock()
+            {
+                ShowViewType = ItemBlockTypeEnum.Label,
+                LabelText = "User Password"
+            });
+            myItemList.Add(new ItemBlock()
+            {
+                ShowViewType = ItemBlockTypeEnum.Entry,
+                LabelText = "Please Enter Password"
+            });
+            myItemList.Add(new ItemBlock()
+            {
+                ShowViewType = ItemBlockTypeEnum.BoxView,
+            });
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
