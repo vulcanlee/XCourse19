bde8c8bd54e4b6e94bc9d76b20c0bc116e2b43a5
李進興 Vulcan <vulcan@miniasp.com>
2019-04-28 16:34:49 +0800

    Prism 資料與命令的資料綁定

---
+++ XF1008/XF1008/XF1008/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF1008.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        #region YourAccount Property
+        private string _YourAccount;
+        public string YourAccount
+        {
+            get { return _YourAccount; }
+            set { SetProperty(ref _YourAccount, value); }
+        }
+        #endregion
+
+        #region YourAnswer Property
+        private string _YourAnswer;
+        public string YourAnswer
+        {
+            get { return _YourAnswer; }
+            set { SetProperty(ref _YourAnswer, value); }
+        }
+        #endregion
+        public DelegateCommand 登入Command { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+
+            登入Command = new DelegateCommand(() =>
+            {
+                YourAnswer = YourAccount;
+            });
+        }
+    }
+}
