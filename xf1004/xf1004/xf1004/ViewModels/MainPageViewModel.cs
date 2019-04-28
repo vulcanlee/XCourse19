8bf7a52826e4adb885975ed8b3f0639261eefc97
vulcan <vulcan.lee@gmail.com>
2019-04-28 10:33:34 +0800

    Xamarin.Forms for Prism 專案練習

---
+++ xf1004/xf1004/xf1004/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xf1004.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string entry { get; set; }
+        public string label { get; set; }
+        public string newlabel { get; set; }
+        public DelegateCommand LoginCommand { get; set; }
+        private readonly INavigationService navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            LoginCommand = new DelegateCommand(MyLogingDel);
+        }
+
+        private void MyLogingDel()
+        {
+            label = entry;
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
