84bfc0473eb08cbac723cee34fe2312d9681ad5d
李進興 Vulcan <vulcan@miniasp.com>
2019-04-28 16:40:18 +0800

    Fody 資料與命令的資料綁定

---
+++ XF1009/XF1009/XF1009/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF1009.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string YourAccount { get; set; }
+        public string YourAnswer { get; set; }
+        public DelegateCommand 登入Command { get; set; }
+        private readonly INavigationService navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            登入Command = new DelegateCommand(() =>
+            {
+                YourAnswer = YourAccount;
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
