b1aa386ebf5aeebbda7bafcc5453fd83dffa984c
vulcan <vulcan.lee@gmail.com>
2019-04-28 10:32:05 +0800

    Xamarin.iOS 原生專案練習

---
+++ xf1002/xf1002/ViewController.designer.cs
@@ -0,0 +1,47 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace xf1002
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel label { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textfield { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (button != null) {
+                button.Dispose ();
+                button = null;
+            }
+
+            if (label != null) {
+                label.Dispose ();
+                label = null;
+            }
+
+            if (textfield != null) {
+                textfield.Dispose ();
+                textfield = null;
+            }
+        }
+    }
+}
