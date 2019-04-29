3012d50d4862b121b2feb37fd8189f69647e397c
李進興 Vulcan <vulcan@miniasp.com>
2019-04-29 14:38:55 +0800

    頁面導航(透過導航頁面) Prism

---
+++ XF2003/XF2003/XF2003/ViewModels/NextPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XF2003.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class NextPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DelegateCommand 回到上一頁Command { get; set; }
+        private readonly INavigationService navigationService;
+
+        public NextPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            回到上一頁Command = new DelegateCommand(() =>
+            {
+                navigationService.GoBackAsync();
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
