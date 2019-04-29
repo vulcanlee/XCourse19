0314d8442d8273a6f415eebc9fd89ca3805bff19
李進興 Vulcan <vulcan@miniasp.com>
2019-04-29 14:17:38 +0800

    頁面導航(透過導航頁面) 後置碼

---
+++ XF2002/XF2002/XF2002/NextPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF2002
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NextPage : ContentPage
+    {
+        public NextPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBack_Clicked(object sender, EventArgs e)
+        {
+            this.Navigation.PopAsync();
+        }
+    }
+}
