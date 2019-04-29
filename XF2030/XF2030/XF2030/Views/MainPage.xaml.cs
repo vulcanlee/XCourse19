d22ffac64c06aacfd3deee721d1efe2091a9761e
李進興 Vulcan <vulcan@miniasp.com>
2019-04-29 15:50:40 +0800

    Xamarin.Forms 的頁面導覽事件

---
+++ XF2030/XF2030/XF2030/Views/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF2030.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Appearing += (s, e) =>
+            {
+                Debug.WriteLine($"  _______>>>  MainPage     Appearing");
+            };
+            this.Disappearing += (s, e) =>
+            {
+                Debug.WriteLine($"  _______>>>  MainPage     Disappearing");
+            };
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Debug.WriteLine($"  _______>>>  MainPage     OnAppearing");
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Debug.WriteLine($"  _______>>>  MainPage     OnDisappearing");
+        }
+    }
+}
---
+++ XF2030/XF2030/XF2030/Views/NextPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace XF2030.Views
+{
+    public partial class NextPage : ContentPage
+    {
+        public NextPage()
+        {
+            InitializeComponent();
+            this.Appearing += (s, e) =>
+            {
+                Debug.WriteLine($"  _______>>>  NextPage     Appearing");
+            };
+            this.Disappearing += (s, e) =>
+            {
+                Debug.WriteLine($"  _______>>>  NextPage     Disappearing");
+            };
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Debug.WriteLine($"  _______>>>  NextPage     OnAppearing");
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Debug.WriteLine($"  _______>>>  NextPage     OnDisappearing");
+        }
+    }
+}
