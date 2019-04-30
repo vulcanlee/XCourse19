e2cb90652a8c20d9dd5ada72b01f3b53e4ef1168
李進興 Vulcan <vulcan@miniasp.com>
2019-04-30 18:34:54 +0800

    頁面導航且傳遞引數 (後置碼)

---
+++ XF2016/XF2016/XF2016/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF2016
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.btn.Clicked += async (s, e) =>
+            {
+                var fooNextPage = new NextPage();
+                // 使用子頁面的公開屬性來設定要傳遞過去的值
+                //fooNextPage.YourName = YourName.Text;
+                // 使用子頁面的公開方法，來設定要傳遞過去的值
+                fooNextPage.SetYourName(YourName.Text);
+
+                await this.Navigation.PushAsync(fooNextPage);
+            };
+        }
+    }
+}
---
+++ XF2016/XF2016/XF2016/NextPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF2016
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NextPage : ContentPage
+    {
+        public NextPage()
+        {
+            InitializeComponent();
+        }
+
+        public void SetYourName(string yourName)
+        {
+            label.Text = yourName;
+        }
+    }
+}
