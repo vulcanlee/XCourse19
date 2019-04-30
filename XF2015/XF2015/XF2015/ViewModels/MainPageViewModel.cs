f7a8f7feaced5153cd58b160f991d4fd6ae42c00
李進興 Vulcan <vulcan@miniasp.com>
2019-04-30 16:51:05 +0800

    Prism 事件2命令的動畫練習

---
+++ XF2015/XF2015/XF2015/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF2015.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public double SliderValue { get; set; }
+        public double ContainerValue { get; set; }
+        public DelegateCommand ValueChangedCommand { get; set; }
+        private readonly INavigationService navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            ValueChangedCommand = new DelegateCommand(() =>
+            {
+                double fooHeight = 294;
+                ContainerValue = fooHeight * (SliderValue / 100.0);
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
