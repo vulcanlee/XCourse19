64d9bda08e9d4750b883e701e924cbb753414c93
李進興 Vulcan <vulcan@miniasp.com>
2019-04-30 16:30:32 +0800

    Prism EventToCommandBehavior

---
+++ XF2014/XF2014/XF2014/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF2014.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DelegateCommand<string> BtnCommand { get; set; }
+        private readonly INavigationService navigationService;
+        private readonly IPageDialogService dialogService;
+
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+        {
+            this.navigationService = navigationService;
+            this.dialogService = dialogService;
+            BtnCommand = new DelegateCommand<string>(async x =>
+            {
+                await dialogService.DisplayAlertAsync("資訊", $"你按下的按鈕是:{x}", "確定");
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
