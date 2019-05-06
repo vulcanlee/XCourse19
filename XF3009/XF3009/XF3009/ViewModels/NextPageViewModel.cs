20f9ca620f87eca6ab08fd70e758004c9cb0ff8e
李進興 Vulcan <vulcan@miniasp.com>
2019-05-06 23:39:58 +0800

    停用軟體與硬體回上頁按鈕

---
+++ XF3009/XF3009/XF3009/ViewModels/NextPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XF3009.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class NextPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DelegateCommand GobackCommand { get; set; }
+        private readonly INavigationService navigationService;
+
+        public NextPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            GobackCommand = new DelegateCommand(() =>
+            {
+                navigationService.GoBackAsync();
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
---
+++ XF3009/XF3009/XF3009/Views/NextPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace XF3009.Views
+{
+    public partial class NextPage : ContentPage
+    {
+        public NextPage()
+        {
+            InitializeComponent();
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
