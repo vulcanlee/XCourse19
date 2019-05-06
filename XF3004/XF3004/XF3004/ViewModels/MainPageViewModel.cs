ec705fe0e20f96a39b3300919d4db3b09e4e02d6
李進興 Vulcan <vulcan@miniasp.com>
2019-05-06 13:45:07 +0800

    不同系統平台下 導航頁面的返回按鈕文字問題

---
+++ XF3004/XF3004/XF3004/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF3004.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Title { get; set; } = "不同系統平台下 導航頁面的返回按鈕文字問題";
+        public string PageTitle { get; set; } = "首頁";
+        public DelegateCommand 測試回上一頁文字Command { get; set; }
+        private readonly INavigationService navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            測試回上一頁文字Command = new DelegateCommand(async () =>
+            {
+                await navigationService.NavigateAsync("Page1");
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
