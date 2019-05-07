184273947313ae39a0d7568f67a81dcdc89af712
李進興 Vulcan <vulcan@miniasp.com>
2019-05-07 23:23:27 +0800

    使用Iconize套件，顯示圖示圖片

--- XF3021/XF3021/XF3021/App.xaml.cs
+++ XF3021/XF3021/XF3021/App.xaml.cs
@@ -4,6 +4,8 @@ using XF3021.ViewModels;
 using XF3021.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System;
+using System.Linq;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace XF3021
@@ -24,9 +26,21 @@ namespace XF3021
             InitializeComponent();
 
             Plugin.Iconize.Iconize
-    .With(new Plugin.Iconize.Fonts.FontAwesomeRegularModule())
-    .With(new Plugin.Iconize.Fonts.FontAwesomeBrandsModule())
-    .With(new Plugin.Iconize.Fonts.FontAwesomeSolidModule());
+                .With(new Plugin.Iconize.Fonts.FontAwesomeRegularModule())
+                .With(new Plugin.Iconize.Fonts.FontAwesomeBrandsModule())
+                .With(new Plugin.Iconize.Fonts.FontAwesomeSolidModule());
+
+            //for (int i = 0; i < Math.Min(Plugin.Iconize.Iconize.Modules.Count, 5); i++)
+            //{
+            //    var module = Plugin.Iconize.Iconize.Modules[i];
+            //    var bc = new ModuleWrapper(module);
+            //    var icon = module.Keys.FirstOrDefault();
+            //    //tabbedPage.Children.Add(new Page1
+            //    //{
+            //    //    BindingContext = bc,
+            //    //    Icon = icon
+            //    //});
+            //}
 
             await NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
