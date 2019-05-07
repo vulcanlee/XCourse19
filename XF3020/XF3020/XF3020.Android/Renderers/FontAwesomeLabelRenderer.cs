150c8c486d3810146b1a2b4acf0dafc4ef8888eb
李進興 Vulcan <vulcan@miniasp.com>
2019-05-07 18:18:33 +0800

    進階應用 Font Awesome

---
+++ XF3020/XF3020/XF3020.Android/Renderers/FontAwesomeLabelRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(XF3020.CustomControls.FontAwesomeLabel), typeof(XF3020.Droid.Renderers.FontAwesomeLabelRenderer))]
+namespace XF3020.Droid.Renderers
+{
+    public class FontAwesomeLabelRenderer : LabelRenderer
+    {
+        public FontAwesomeLabelRenderer(Context context) : base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            var label = Control;
+            Typeface font;
+            try
+            {
+                font = Typeface.CreateFromAsset(Forms.Context.Assets, "fontawesome.ttf");
+                label.Typeface = font;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("TTF file not found. Make sure the Android project contains it at 'fontawesome.ttf'.");
+            }
+        }
+    }
+}
---
+++ XF3020/XF3020/XF3020/CustomControls/FontAwesomeLabel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XF3020.CustomControls
+{
+    public class FontAwesomeLabel : Label
+    {
+        public FontAwesomeLabel()
+        {
+            // 這個用法已經過程，要改用 Switch 的方法
+            //FontFamily = Device.OnPlatform(
+            //    iOS:  "fontawesome", 
+            //    Android: "fontawesome", 
+            //    WinPhone:"fontawesome.ttf#FontAwesome");
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.UWP:
+                    FontFamily = @"/fontawesome.ttf#FontAwesome";
+                    break;
+                case Device.iOS:
+                    FontFamily = @"fontawesome";
+                    break;
+                case Device.Android:
+                    FontFamily = @"fontawesome.ttf";
+                    break;
+                default:
+                    FontFamily = "fontawesome";
+                    break;
+            }
+        }
+    }
+}
