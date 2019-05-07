07e06009c37d96f35b9c5265f8e486d456fb7fbc
李進興 Vulcan <vulcan@miniasp.com>
2019-05-07 19:38:46 +0800

    使用Iconize套件，顯示圖示圖片

---
+++ XF3021/XF3021/XF3021.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Prism;
+using Prism.Ioc;
+
+namespace XF3021.Droid
+{
+    [Activity(Label = "XF3021", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            #region 進行 Iconize 套件的初始化
+            //Plugin.Iconize.Iconize.Init(Resource.Id.toolbar, Resource.Id.tabs);
+            Plugin.Iconize.Iconize.Init(Resource.Id.toolbar, Resource.Id.sliding_tabs);
+            #endregion
+
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
+
---
+++ XF3021/XF3021/XF3021/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Prism;
+using Prism.Ioc;
+using XF3021.ViewModels;
+using XF3021.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XF3021
+{
+    public partial class App
+    {
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            Plugin.Iconize.Iconize
+    .With(new Plugin.Iconize.Fonts.FontAwesomeRegularModule())
+    .With(new Plugin.Iconize.Fonts.FontAwesomeBrandsModule())
+    .With(new Plugin.Iconize.Fonts.FontAwesomeSolidModule());
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
