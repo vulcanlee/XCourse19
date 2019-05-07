df7d2a73f30a6c48e49d76f0a6373f559e6478a4
李進興 Vulcan <vulcan@miniasp.com>
2019-05-07 11:22:57 +0800

    Grid 版面配置練習

---
+++ XF3010/XF3010/XF3010/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF3010.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public double CurrentFontSize { get; set; } = 20;
+        public DelegateCommand<string> ChangeFontSizeCommand { get; set; }
+        private readonly INavigationService navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            ChangeFontSizeCommand = new DelegateCommand<string>(x =>
+            {
+                if (x == "1")
+                {
+                    CurrentFontSize += 4;
+                }
+                else
+                {
+                    CurrentFontSize -= 4;
+                }
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
