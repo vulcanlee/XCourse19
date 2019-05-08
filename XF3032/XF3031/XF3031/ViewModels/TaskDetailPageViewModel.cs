8506e35fb550fd29755dfc2674e87b1688ba20d6
李進興 Vulcan <vulcan@miniasp.com>
2019-05-08 19:48:53 +0800

    集合資料CRUD 模擬情境2

---
+++ XF3032/XF3031/XF3031/ViewModels/TaskDetailPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XF3031.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    using XF3031.Models;
+
+    public class TaskDetailPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MyTaskItem MyTaskItemSelected { get; set; }
+        public string TaskMode { get; set; }
+        public DelegateCommand DeleteCommand { get; set; }
+        public DelegateCommand SaveCommand { get; set; }
+        private readonly INavigationService navigationService;
+
+        public TaskDetailPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("Mode"))
+            {
+                TaskMode = parameters.GetValue<string>("Mode");
+                if (TaskMode == "Edit")
+                {
+                    MyTaskItemSelected = parameters.GetValue< MyTaskItem>("MyTaskItem");
+                }
+                else
+                {
+                    MyTaskItemSelected = new MyTaskItem();
+                }
+            }
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
