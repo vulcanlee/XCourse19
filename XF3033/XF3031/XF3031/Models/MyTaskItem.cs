9ecf3cc30b635f2c858bd86c43dda858e510fddf
李進興 Vulcan <vulcan@miniasp.com>
2019-05-08 20:08:34 +0800

    集合資料CRUD 修改與更新3

---
+++ XF3033/XF3031/XF3031/Models/MyTaskItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace XF3031.Models
+{
+    public class MyTaskItem : INotifyPropertyChanged, ICloneable
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        //工作名稱
+        public string MyTaskName { get; set; }
+        //狀態
+        public string MyTaskStatus { get; set; }
+        //指派日期
+        public DateTime MyTaskDate { get; set; }
+
+        public MyTaskItem Clone()
+        {
+            var fooObject = ((ICloneable)this).Clone();
+            return fooObject as MyTaskItem;
+        }
+
+        object ICloneable.Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ XF3033/XF3031/XF3031/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,109 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF3031.ViewModels
+{
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    using XF3031.Models;
+    using XF3031.Repositories;
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MyTaskItem MyTaskItemSelected { get; set; }
+        public bool RefreshingStatus { get; set; }
+        public ObservableCollection<MyTaskItem> MyTaskItemList { get; set; } = new ObservableCollection<MyTaskItem>();
+        public int RefreshIndex { get; set; } = 0;
+        public DelegateCommand MyTaskItemSelectedCommand { get; set; }
+        public DelegateCommand MyTaskRefreshCommand { get; set; }
+        public DelegateCommand AddCommand { get; set; }
+        private readonly INavigationService navigationService;
+        private readonly IPageDialogService dialogService;
+
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+        {
+            this.navigationService = navigationService;
+            this.dialogService = dialogService;
+            MyTaskItemSelectedCommand = new DelegateCommand(async () =>
+            {
+                //await _dialogService.DisplayAlertAsync("Info", $"你選取的是 {MyTaskItemSelected.MyTaskName}", "OK");
+
+                NavigationParameters fooPara = new NavigationParameters();
+                fooPara.Add("MyTaskItem", MyTaskItemSelected.Clone());
+                fooPara.Add("Mode", "Edit");
+                await navigationService.NavigateAsync("TaskDetailPage", fooPara);
+            });
+
+            MyTaskRefreshCommand = new DelegateCommand(() =>
+            {
+                RefreshIndex++;
+                MyTaskItemList.Clear();
+                MyTaskRepository fooMyTaskRepository = new MyTaskRepository();
+                var fooTasks = fooMyTaskRepository.GetMyTask();
+                foreach (var item in fooTasks)
+                {
+                    MyTaskItemList.Add(new MyTaskItem
+                    {
+                        MyTaskName = $"{RefreshIndex} {item.MyTaskName}",
+                        MyTaskDate = item.MyTaskDate,
+                        MyTaskStatus = item.MyTaskStatus,
+                    });
+                }
+                RefreshingStatus = false;
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            if (parameters.GetNavigationMode() == NavigationMode.New)
+            {
+                MyTaskRepository fooMyTaskRepository = new MyTaskRepository();
+                var fooTasks = fooMyTaskRepository.GetMyTask();
+                foreach (var item in fooTasks)
+                {
+                    MyTaskItemList.Add(new MyTaskItem
+                    {
+                        MyTaskName = item.MyTaskName,
+                        MyTaskDate = item.MyTaskDate,
+                        MyTaskStatus = item.MyTaskStatus,
+                    });
+                }
+            }
+            else
+            {
+                if (parameters.ContainsKey("Mode"))
+                {
+                    string TaskMode = parameters.GetValue<string>("Mode");
+                    var fooObject = parameters.GetValue<MyTaskItem>("MyTaskItem");
+                    var index = MyTaskItemList.IndexOf(MyTaskItemList.FirstOrDefault(x => x.MyTaskName == fooObject.MyTaskName));
+                    if (TaskMode == "Update")
+                    {
+                        MyTaskItemList[index] = fooObject;
+                    }
+                    else
+                    {
+                        MyTaskItemList.RemoveAt(index);
+                    }
+                }
+            }
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
