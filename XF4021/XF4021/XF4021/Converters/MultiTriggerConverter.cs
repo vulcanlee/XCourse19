daa13aef1de321d430cf52666300e2e43828fcb5
李進興 Vulcan <vulcan@miniasp.com>
2019-05-11 00:13:14 +0800

    範例體驗 : 觸發 Trigger

---
+++ XF4021/XF4021/XF4021/Converters/MultiTriggerConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XF4021.Converters
+{
+    /// <summary>
+    /// 建立一個數值轉換器，將傳入的整數數值，轉換成為布林型態的值
+    /// </summary>
+    class MultiTriggerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if ((int)value > 0)
+                return true;    // data has been entered
+            else
+                return false;   // input is empty
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ XF4021/XF4021/XF4021/Triggers/NumericValidationTriggerAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XF4021.Triggers
+{
+    /// <summary>
+    /// 定義Trigger條件滿足時候，要執行使用者自訂的動作
+    /// </summary>
+    class NumericValidationTriggerAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry entry)
+        {
+            //請在這裡定義使用者要執行的動作
+            double result;
+            bool isValid = Double.TryParse(entry.Text, out result);
+            entry.TextColor = isValid ? Color.Default : Color.Red;
+            entry.Scale = 1.4;
+        }
+    }
+}
