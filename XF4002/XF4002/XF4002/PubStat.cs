873a7b1aade39d1341ef3f52761edbf7ba73ee97
李進興 Vulcan <vulcan@miniasp.com>
2019-05-10 11:10:46 +0800

    範例：XAML 靜態與動態資源的使用

---
+++ XF4002/XF4002/XF4002/PubStat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XF4002
+{
+    public class PubStat
+    {
+        public static Color 文字顏色 { get; set; } = Color.Red;
+        public static Color 背景顏色 { get; set; } = Color.Yellow;
+        public static double 文字尺寸 { get; set; } = 20;
+    }
+}
