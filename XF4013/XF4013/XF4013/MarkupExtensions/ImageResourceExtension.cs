6df412cd3d222868eeace4e9edcecb138513e0a2
李進興 Vulcan <vulcan@miniasp.com>
2019-05-10 10:41:50 +0800

    延伸標記之客制擴充 IMarkupExtension

---
+++ XF4013/XF4013/XF4013/MarkupExtensions/ImageResourceExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF4013.MarkupExtensions
+{
+    [ContentProperty("Source")]
+    class ImageResourceExtension : IMarkupExtension<ImageSource>
+    {
+        public string Source { set; get; }
+
+        public ImageSource ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (String.IsNullOrEmpty(Source))
+            {
+                IXmlLineInfoProvider lineInfoProvider = serviceProvider.GetService(typeof(IXmlLineInfoProvider)) as IXmlLineInfoProvider;
+                IXmlLineInfo lineInfo = (lineInfoProvider != null) ? lineInfoProvider.XmlLineInfo : new XmlLineInfo();
+                throw new XamlParseException("ImageResourceExtension requires Source property to be set", lineInfo);
+            }
+
+            string assemblyName = GetType().GetTypeInfo().Assembly.GetName().Name;
+            //string fooImageSourceName = assemblyName + $".Images." + Source;
+            string fooImageSourceName = assemblyName + "." + Source;
+            Assembly fooSourceAssembly = typeof(ImageResourceExtension).GetTypeInfo().Assembly;
+            return ImageSource.FromResource(fooImageSourceName, fooSourceAssembly);
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (this as IMarkupExtension<ImageSource>).ProvideValue(serviceProvider);
+        }
+    }
+}
