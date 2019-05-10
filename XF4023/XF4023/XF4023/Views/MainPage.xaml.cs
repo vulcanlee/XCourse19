f996bfa3a18b6656fca0b6ed1a4d450e75b5a668
李進興 Vulcan <vulcan@miniasp.com>
2019-05-11 01:20:15 +0800

    範例體驗 : 手勢操作 GestureRecognizer

---
+++ XF4023/XF4023/XF4023/Views/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XF4023.ViewModels;
+
+namespace XF4023.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        MainPageViewModel MainPageViewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            MainPageViewModel = this.BindingContext as MainPageViewModel;
+        }
+
+        private void OnPinch(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            if (e.Scale > 1.0)
+            {
+                MainPageViewModel.Title = $"放大 {e.Scale}";
+            }
+            else
+            {
+                MainPageViewModel.Title = $"縮小 {e.Scale}";
+            }
+        }
+
+        private void OnPan(object sender, PanUpdatedEventArgs e)
+        {
+            MainPageViewModel.Title = $"平移 X:{e.TotalX} Y:{e.TotalY}";
+        }
+
+        private void OnSwiped(object sender, SwipedEventArgs e)
+        {
+            MainPageViewModel.Title = $"滑動 {e.Direction}";            
+        }
+    }
+}
