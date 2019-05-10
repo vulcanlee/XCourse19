3541af2349833e76fb504b4913c9553771c1040c
李進興 Vulcan <vulcan@miniasp.com>
2019-05-10 14:37:21 +0800

    範例： XAML 的 樣式 style 應用(資源合併)

---
+++ XF4007/XF4007/XF4007/ViewModels/Page2ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XF4007.ViewModels
+{
+    public class Page2ViewModel : BindableBase
+    {
+        public Page2ViewModel()
+        {
+
+        }
+    }
+}
---
+++ XF4007/XF4007/XF4007/Views/Page2.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XF4007.Views
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
