c39fb1d8a8643ba6fd3e7da2f23d862480210152
李進興 Vulcan <vulcan@miniasp.com>
2019-05-11 11:31:47 +0800

    請試著找出執行會閃退的錯誤，並修正

---
+++ XF6002/XF4027/XF4027/ViewModels/NewPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XF4027.ViewModels
+{
+    public class NewPageViewModel : BindableBase
+    {
+        public NewPageViewModel()
+        {
+
+        }
+    }
+}
---
+++ XF6002/XF4027/XF4027/Views/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XF4027.ViewModels;
+
+namespace XF4027.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        MainPageViewModel fooMainPageViewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            fooMainPageViewModel = this.BindingContext as MainPageViewModel;
+            fooMainPageViewModel.串接動畫Delegate = 串接動畫;
+            fooMainPageViewModel.各自動畫Delegate = 各自動畫;
+            fooMainPageViewModel.頁面消失動畫Delegate = 頁面消失動畫;
+        }
+
+        private async Task 頁面消失動畫()
+        {
+            await 頁面收起來();
+        }
+
+        public async Task 各自動畫()
+        {
+            // 使用 TPL 的 Task.WhenAll，讓所有的動畫都同時運行
+            await Task.WhenAll(
+                button.TranslateTo(200, 0, 1000),
+                button.TranslateTo(0, 100, 1000),
+                button.ScaleTo(2.0, 1000),
+                button.RotateTo(360, 1000));
+        }
+        public async Task 串接動畫()
+        {
+            //在這裡，每個動畫會依序逐步執行
+            await button.TranslateTo(200, 0, 1000);
+            await button.TranslateTo(0, 100, 1000);
+            await button.ScaleTo(2.0, 1000);
+            await button.RotateTo(360, 1000);
+        }
+
+        protected async override void OnAppearing()
+        {
+            await 頁面攤開來();
+            base.OnAppearing();
+        }
+
+        protected async override void OnDisappearing()
+        {
+            await 頁面收起來();
+            base.OnDisappearing();
+        }
+
+        /// <summary>
+        /// 當頁面要隱藏起來的時候，會執行這個動畫效果
+        /// </summary>
+        /// <returns></returns>
+        public async Task 頁面收起來()
+        {
+            this.AnchorX = 0;
+            this.RotationY = 0;
+            await this.RotateYTo(-180, 2000);
+            this.AnchorX = 0.5;
+            this.RotationY = 0;
+        }
+
+        /// <summary>
+        /// 當頁面要顯示出來的時候，會顯示這個動畫效果
+        /// </summary>
+        /// <returns></returns>
+        public async Task 頁面攤開來()
+        {
+            this.AnchorX = 0;
+            this.RotationY = -180;
+            await this.RotateYTo(0, 2000);
+            this.AnchorX = 0.5;
+            this.RotationY = 0;
+        }
+    }
+}
