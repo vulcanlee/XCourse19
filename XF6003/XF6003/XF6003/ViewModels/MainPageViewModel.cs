8efec91cd084f7512bdc8accc64b7d1b8953612a
李進興 Vulcan <vulcan@miniasp.com>
2019-05-19 22:26:47 +0800

    請修正這個應用程式會閃退的問題

---
+++ XF6003/XF6003/XF6003/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF6003.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DelegateCommand SetAlertCommand { get; set; }
+        public int AfterMS { get; set; } = 10;
+        public string Title { get; set; } = "提醒";
+        public string Message { get; set; } = "您的預約通知已經到時了";
+        private readonly INavigationService navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            SetAlertCommand = new DelegateCommand(() =>
+            {
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
