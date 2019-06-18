682693c3d0be7fc4cb9dd80c882cc702f7ba7680
李進興 Vulcan <vulcan@miniasp.com>
2019-06-18 18:42:38 +0800

    使用抽象型別 介面 來解決緊密耦合問題

---
+++ XF7006/XF7006/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF7006
+{
+    interface IMessageWriter
+    {
+        void Output();
+    }
+    class ConsoleWriter : IMessageWriter
+    {
+        public void Output()
+        {
+            Console.WriteLine($"ConsoleWriter 執行結果");
+        }
+    }
+    class FileWriter : IMessageWriter
+    {
+        public void Output()
+        {
+            Console.WriteLine($"FileWriter 執行結果");
+        }
+    }
+    class MyClass
+    {
+        private readonly IMessageWriter messageWriter;
+
+        public MyClass(IMessageWriter messageWriter)
+        {
+            this.messageWriter = messageWriter;
+        }
+        public void DoSomething()
+        {
+            messageWriter.Output();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessageWriter messageWriter = new ConsoleWriter();
+            MyClass myClass = new MyClass(messageWriter);
+            myClass.DoSomething();
+
+            messageWriter = new FileWriter();
+            myClass = new MyClass(messageWriter);
+            myClass.DoSomething();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
