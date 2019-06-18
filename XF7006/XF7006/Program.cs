b0996f5da1ecbd35531ff8e0d15ba186ccabee3c
李進興 Vulcan <vulcan@miniasp.com>
2019-06-18 20:51:33 +0800

    使用抽象型別 介面 來解決緊密耦合問題

--- XF7006/XF7006/Program.cs
+++ XF7006/XF7006/Program.cs
@@ -14,26 +14,32 @@ namespace XF7006
     {
         public void Output()
         {
-            Console.WriteLine($"ConsoleWriter 執行結果");
+            Console.WriteLine($"   ConsoleWriter 執行結果");
         }
     }
     class FileWriter : IMessageWriter
     {
         public void Output()
         {
-            Console.WriteLine($"FileWriter 執行結果");
+            Console.WriteLine($"   FileWriter 執行結果");
         }
     }
     class MyClass
     {
         private readonly IMessageWriter messageWriter;
 
+        // 用戶端會注入所需要的 IMessageWriter 實作物件
         public MyClass(IMessageWriter messageWriter)
         {
+            // 透過相依於抽象 介面 型別，解除與
+            // 相關類別的緊密耦合關係
+            // 日後僅需要新增新類別，而不需修改 MyClass
+            // 就可以完成變更需求的工作
             this.messageWriter = messageWriter;
         }
         public void DoSomething()
         {
+            Console.WriteLine("DoSomething 正在執行中");
             messageWriter.Output();
         }
     }
---
+++ XF7007/XF7007/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF7007
+{
+    class ConsoleWriter
+    {
+        public void Output()
+        {
+            Console.WriteLine($"   ConsoleWriter 執行結果");
+        }
+    }
+    class MyClass
+    {
+        private readonly ConsoleWriter consoleWriter;
+        public MyClass()
+        {
+            consoleWriter = new ConsoleWriter();
+        }
+        public void DoSomething()
+        {
+            Console.WriteLine("DoSomething 正在執行中");
+            consoleWriter.Output();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.DoSomething();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
