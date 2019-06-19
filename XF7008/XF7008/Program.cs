294ccbcbe0f82e82310f759adde018bb7a36f6ad
李進興 Vulcan <vulcan@miniasp.com>
2019-06-19 10:55:51 +0800

    練習情境 : 使用欄位和方法，建立一個學生類別

---
+++ XF7008/XF7008/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF7008
+{
+    class Student
+    {
+        string name;
+        int age;
+        int grade;
+        string department;
+        public void Display()
+        {
+            Console.WriteLine($"{name}/{age}/{grade}/{department}");
+        }
+        public void Init(string name, int age, int grade, string department)
+        {
+            this.name = name;
+            this.age = age;
+            this.grade = grade;
+            this.department = department;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Init("Vulcan", 19, 2, "CS");
+            student.Display();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
