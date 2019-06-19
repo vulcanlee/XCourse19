c8319690ae7c5a2ce13d5a6cd25322d4077c0244
李進興 Vulcan <vulcan@miniasp.com>
2019-06-19 11:29:05 +0800

    練習情境 : 使用自動建置屬性和方法，建立一個學生類別

---
+++ XF7010/XF7010/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF7010
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Grade { get; set; }
+        public string Department { get; set; }
+        public Student(string name, int age,
+            int grade, string department)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+            Department = department;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{Name}/{Age}/" +
+                $"{Grade}/{Department}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Vulcan", 19, 2, "CS");
+            student.Display();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
