d6bad75a63e6a00f4aed8d0cf4c48b287ad63331
李進興 Vulcan <vulcan@miniasp.com>
2019-06-27 16:22:24 +0800

    Lambda之非同步方法使用

---
+++ XF7013/XF7013/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XF7013
+{
+    class Program
+    {
+        static Action<string> MyHandler;
+        static Action MyNoArgumentHandler;
+        static void Main(string[] args)
+        {
+            MyHandler = async x =>
+            {
+                await Task.Run(() =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine(x);
+                });
+            };
+            MyHandler("Vulcan");
+            MyNoArgumentHandler = async () =>
+            {
+                await Task.Run(() =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("沒有參數的呼叫");
+                });
+            };
+            MyNoArgumentHandler();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();  
+        }
+    }
+}
