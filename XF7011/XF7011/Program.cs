d40fd93aeac35b8a47c8bf394abe8e3e46c16374
李進興 Vulcan <vulcan@miniasp.com>
2019-06-27 15:59:53 +0800

    內建的委派 - Action 範例程式碼

---
+++ XF7011/XF7011/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF7011
+{
+    delegate void MyActionDelegate(string message);
+    class MyActionClass
+    {
+        // 使用 Action 宣告一個委派變數
+        Action<string> MyActionHandler;
+        // 使用 delegate 自訂型別來宣告一個委派變數
+        MyActionDelegate MyDelegateHandler;
+        public MyActionClass()
+        {
+            MyActionHandler = this.MyInstanceMethod;
+            MyActionHandler = MyActionClass.MyStaticMethod;
+            MyActionHandler = this.MyInstanceMethod;
+            MyActionHandler = MyActionClass.MyStaticMethod;
+        }
+        // 執行個體方法
+        public void MyInstanceMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+        // 靜態方法
+        public static void MyStaticMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
