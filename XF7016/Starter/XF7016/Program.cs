74fedd7f900d90b10ecb1a49f0f79c4e6f344dc0
李進興 Vulcan <vulcan@miniasp.com>
2019-06-28 18:03:07 +0800

    使用委派來設計事件機制

---
+++ XF7016/Starter/XF7016/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XF7016
+{
+    class Car
+    {
+        int gas = 10;
+        public void Run()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                gas--;
+                if(gas == 0)
+                {
+                    Console.WriteLine($"已經沒有汽油了，無法繼續行駛");
+                    return;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Run();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
