1b31b848d613c8ca88b30f57ef9a8557e08a5af8
李進興 Vulcan <vulcan@miniasp.com>
2019-06-28 23:04:47 +0800

    委派變數可以列出所有委派方法，事件做不到

---
+++ XF7021/XF7021/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF7021
+{
+    class MyDelegateVSEvent
+    {
+        public Action<string> MyHandler;
+        public event Action<string> MyEventHandler;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegateVSEvent myDelegateVSEvent = new MyDelegateVSEvent();
+            myDelegateVSEvent.MyHandler += MyMethod;
+            Console.WriteLine($"委派方法數量 {myDelegateVSEvent.MyHandler.GetInvocationList().Count()}");
+            myDelegateVSEvent.MyEventHandler += MyMethod;
+            // 對於 event 不能夠在類別外取得委派方法清單
+            //Console.WriteLine($"事件方法數量 {myDelegateVSEvent.MyEventHandler.GetInvocationList().Count()}");
+        }
+        static void MyMethod(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+    }
+}
