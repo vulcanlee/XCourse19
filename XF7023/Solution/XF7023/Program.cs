4df728785891dae988c369d2851dfd4622868613
李進興 Vulcan <vulcan@miniasp.com>
2019-06-29 11:49:42 +0800

    練習情境 : 多執行緒的智慧型溫溫度計

---
+++ XF7023/Solution/XF7023/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XF7023
+{
+    class HighTemperatureEventArgs : EventArgs
+    {
+        public int CurrentTemperature { get; set; }
+    }
+    class SmartThermometer
+    {
+        Random random = new Random();
+        bool RunningStatus = false;
+        public event EventHandler<HighTemperatureEventArgs> HighTemperatureHandler;
+        int GetCurrentTemperature() => random.Next(15, 50);
+        public void TurnOn()
+        {
+            RunningStatus = true;
+            ThreadPool.QueueUserWorkItem(x =>
+            {
+                while (RunningStatus)
+                {
+                    var currentTemperature = GetCurrentTemperature();
+                    Console.WriteLine($"現在溫度 {currentTemperature} 度");
+                    if(currentTemperature>=40)
+                    {
+                        RaiseHighTemperatureAlert(currentTemperature);
+                    }
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+        void RaiseHighTemperatureAlert(int currentTemperature)
+        {
+            //var handler = HighTemperatureHandler;
+            //if (handler != null)
+            //{
+            //    handler(this, new HighTemperatureEventArgs()
+            //    {
+            //        CurrentTemperature = currentTemperature
+            //    });
+            //}
+
+            HighTemperatureHandler?.Invoke(this, new HighTemperatureEventArgs()
+            {
+                CurrentTemperature = currentTemperature
+            });
+        }
+        public void TurnOff() => RunningStatus = false;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartThermometer smartThermometer = new SmartThermometer();
+            smartThermometer.HighTemperatureHandler += (s, e) =>
+                {
+                    Console.WriteLine($"   警告，高溫出現，現在溫度為 {e.CurrentTemperature} 度");
+                };
+            smartThermometer.TurnOn();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ XF7023/Starter/XF7023/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XF7023
+{
+    class SmartThermometer
+    {
+        Random random = new Random();
+        bool RunningStatus = false;
+        int GetCurrentTemperature() => random.Next(15, 50);
+        public void TurnOn()
+        {
+            RunningStatus = true;
+            ThreadPool.QueueUserWorkItem(x =>
+            {
+                while(RunningStatus)
+                {
+                    var currentTemperature = GetCurrentTemperature();
+                    Console.WriteLine($"現在溫度 {currentTemperature} 度");
+
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+        public void TurnOff() => RunningStatus = false;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartThermometer smartThermometer = new SmartThermometer();
+            smartThermometer.TurnOn();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
